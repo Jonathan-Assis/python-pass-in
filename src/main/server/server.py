84f1c5758562eea26cf1e19a47c437f614354487
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2024-04-07 17:52:31 -0300

    Added check in routes

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,6 +1,7 @@
 from src.models.settings.connection import db_connection_handler
 from src.main.routes.event_routes import event_route_bp
 from src.main.routes.attendees_routes import attendees_route_bp
+from src.main.routes.check_in_routes import check_in_route_bp
 from flask import Flask
 from flask_cors import CORS
 
@@ -11,3 +12,4 @@ CORS(app)
 
 app.register_blueprint(event_route_bp)
 app.register_blueprint(attendees_route_bp)
+app.register_blueprint(check_in_route_bp)
